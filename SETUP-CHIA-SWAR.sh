62eba186085b5ad13e4503a8eb168d566f83d60c
Rahim Khoja <rahim@khoja.ca>
2021-05-30 14:21:18 -0700

    Update SETUP-CHIA-SWAR.sh

--- SETUP-CHIA-SWAR.sh
+++ SETUP-CHIA-SWAR.sh
@@ -65,6 +65,8 @@ cd /usr/share
 git clone https://github.com/swar/Swar-Chia-Plot-Manager.git
 chmod -R 777 /usr/share/Swar-Chia-Plot-Manager
 sudo -u storage -- sh -c "cd /usr/share/Swar-Chia-Plot-Manager; /usr/bin/pip3 install -r requirements.txt"
+sudo -u storage -- sh -c "cd /usr/share/Swar-Chia-Plot-Manager; /usr/bin/pip3 install -r requirements-notification.txt"
+
 
 cp /usr/share/Swar-Chia-Plot-Manager/config.yaml.default /usr/share/Swar-Chia-Plot-Manager/config.yaml
 
