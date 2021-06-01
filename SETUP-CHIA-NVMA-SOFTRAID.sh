3d3f349f5fc4cc8a732a6f62c1796929c990cb8e
Rahim Khoja <rahim@khoja.ca>
2021-05-31 17:00:19 -0700

    Update and rename SETUP-CHIA-DISKS.sh to SETUP-CHIA-NVMA-SOFTRAID.sh

--- SETUP-CHIA-NVMA-SOFTRAID.sh
+++ SETUP-CHIA-NVMA-SOFTRAID.sh
@@ -52,6 +52,7 @@ then
     exit 1
 fi
 
+# Get the user that ran the Script (Not Root)
 baseuser=$(pstree -lu -s $$ | grep --max-count=1 -o '([^)]*)' | head -n 1 | sed 's/[()]//g') # Run commands in brackets and save output to variable`
 
 # Setup NVME Soft Raid. (This uses two NVME drives, feel free to modify for your own needs)
@@ -70,58 +71,12 @@ echo '/dev/md0 /mnt/plot ext4 defaults,nofail 0 0' | tee -a /etc/fstab
 cat /proc/mdstat
 chmod -R 777 /mnt/plot/
 
+# Setup FSTrim Timer for Hourly.
+cp etc/systemd/system/timers.target.wants/fstrim.timer /etc/systemd/system/timers.target.wants/fstrim.timer
 systemctl daemon-reload
+
+# Enable FSTrim Timer
 systemctl enable fstrim.timer
 
-
-wipefs /dev/sdb
-wipefs /dev/sdc
-wipefs /dev/sdd
-wipefs /dev/sde
-wipefs /dev/sdf
-wipefs /dev/sdg
-wipefs /dev/sdh
-wipefs /dev/sdi
-wipefs /dev/sdj
-wipefs /dev/sdk
-wipefs /dev/sdl
-
-zpool create r0zfs1 /dev/sdb -f
-zpool create r0zfs2 /dev/sdc -f
-zpool create r0zfs3 /dev/sdd -f
-zpool create r0zfs4 /dev/sde -f
-zpool create r0zfs5 /dev/sdf -f
-zpool create r0zfs6 /dev/sdg -f
-zpool create r0zfs7 /dev/sdh -f
-zpool create r0zfs8 /dev/sdi -f
-zpool create r0zfs9 /dev/sdj -f
-zpool create r0zfs10 /dev/sdk -f
-zpool create r0zfs11 /dev/sdl -f
-
-chmod -R 777 /r0zfs1
-chmod -R 777 /r0zfs2
-chmod -R 777 /r0zfs3
-chmod -R 777 /r0zfs4
-chmod -R 777 /r0zfs5
-chmod -R 777 /r0zfs6
-chmod -R 777 /r0zfs7
-chmod -R 777 /r0zfs8
-chmod -R 777 /r0zfs9
-chmod -R 777 /r0zfs10
-chmod -R 777 /r0zfs11
-
-mkdir /r0zfs0
-chmod -R 777 /r0zfs0
-
-apt install nfs-kernel-server -y
-zfs set sharenfs="on" r0zfs1
-zfs set sharenfs="on" r0zfs2
-zfs set sharenfs="on" r0zfs3
-zfs set sharenfs="on" r0zfs4
-zfs set sharenfs="on" r0zfs5
-zfs set sharenfs="on" r0zfs6
-zfs set sharenfs="on" r0zfs7
-zfs set sharenfs="on" r0zfs8
-zfs set sharenfs="on" r0zfs9
-zfs set sharenfs="on" r0zfs10
-zfs set sharenfs="on" r0zfs11
+# Remount with FSTab
+mount -a
