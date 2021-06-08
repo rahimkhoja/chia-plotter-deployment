d4d9e724d569ab005f54c01812588a14793edd1b
Rahim Khoja <rahim@khoja.ca>
2021-06-08 00:43:54 -0700

    Update SETUP-CHIA-PLOTTER2.sh

--- SETUP-CHIA-PLOTTER2.sh
+++ SETUP-CHIA-PLOTTER2.sh
@@ -114,10 +114,13 @@ chmod -R 777 /var/log/chia-plotter
 
 while true
 do
-    # Create Plot
+    # Sleep For 10 Seconds
+    sleep 10
+    
+    # Create Plot with 29 Threads 
     chia_plot a0226aee1c59ba9a70f9c4aab77ea662bce96766b58913dfff2fb48c5766d594d0430b38df7b5f9f1787d39d7ab310f6 9425d29b05b659d05814cd989e0d976c9b755f9730d3e112c8609aa3d5dd250a86ecb8c164a66861857d45e71d6c9b9a /mnt/plot/ /mnt/plot/ 29 7 2>&1 /var/log/chia-plotter/plotter.log
 
-    # Sleep 10 Seconds
+    # Sleep For 10 Seconds
     sleep 10
 
     # Copy Plot to ZFS Disk & Delete Plot on NVME When Copied
