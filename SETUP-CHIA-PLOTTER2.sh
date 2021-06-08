b823695a78b12470c33f51a5d7bd4be5441828e2
Rahim Khoja <rahim@khoja.ca>
2021-06-08 01:12:50 -0700

    Update SETUP-CHIA-PLOTTER2.sh

--- SETUP-CHIA-PLOTTER2.sh
+++ SETUP-CHIA-PLOTTER2.sh
@@ -92,8 +92,20 @@ echo "/dev/nvme0n1p1   /mnt/plot       ext4    defaults        0       0" >> /et
 mount -a
 chmod -R 777 /mnt/plot
 
+# Create Logging Directory 
+mkdir -p /var/log/chia-plotter
+chmod -R 777 /var/log/chia-plotter
+
 # Install Telegram-Send
 pip3 install telegram-send
+
+# Install Plotter Service
+cp usr/local/bin/madmax-plot.sh /usr/local/bin/
+chmod +x /usr/local/bin/madmax-plot.sh
+cp etc/systemd/system/plotter.service /etc/systemd/system/
+
+# Install Updated FSTrim Timer Service ( Hourly )
+cp etc/systemd/system/timers.target.wants/ /etc/systemd/system/timers.target.wants/fstrim.timer
 
 # Setup and Compile MadMax Chia Plotter
 cd /usr/share
@@ -109,23 +121,11 @@ zpool create r0zfs1 /dev/sdb -f
 chmod -R 777 /r0zfs1
 zfs set sharenfs="on" r0zfs1
 
-# Create Logging Directory 
-mkdir -p /var/log/chia-plotter
-chmod -R 777 /var/log/chia-plotter
+# Reload Systemd Daemons
+systemctl daemon-reload
 
-# This should be on startup .. will fix soon
-while true
-do
-    # Sleep For 10 Seconds
-    sleep 10
-    
-    # Create Plot with 29 Threads 
-    chia_plot a0226aee1c59ba9a70f9c4aab77ea662bce96766b58913dfff2fb48c5766d594d0430b38df7b5f9f1787d39d7ab310f6 9425d29b05b659d05814cd989e0d976c9b755f9730d3e112c8609aa3d5dd250a86ecb8c164a66861857d45e71d6c9b9a /mnt/plot/ /mnt/plot/ 29 7 2>&1 /var/log/chia-plotter/plotter.log
+# Enable FSTrim Service
+systemctl enable fstrim.timer
 
-    # Sleep For 10 Seconds
-    sleep 10
-
-    # Copy Plot to ZFS Disk & Delete Plot on NVME When Copied
-    rsync -v --remove-source-files --info=progress2 /mnt/plot/*.plot /r0zfs1/ 2>&1 /var/log/chia-plotter/plotter.log
-done
-
+# Enable Plotter Service
+systemctl enable plotter.service
