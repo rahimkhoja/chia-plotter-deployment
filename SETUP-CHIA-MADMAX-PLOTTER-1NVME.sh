41dac442333d8cff57cfaec03bcb48c087cffe18
Rahim Khoja <rahim@khoja.ca>
2021-06-09 14:23:19 -0700

    Update SETUP-CHIA-MADMAX-PLOTTER-1NVME.sh

--- SETUP-CHIA-MADMAX-PLOTTER-1NVME.sh
+++ SETUP-CHIA-MADMAX-PLOTTER-1NVME.sh
@@ -132,8 +132,13 @@ systemctl enable fstrim.timer
 # Enable Plotter Service
 systemctl enable plotter.service
 
-
+num_cpus=$(nproc)
 
 echo
 echo "Deployment Successful"
-echo "You Have 
+echo
+echo "You Have ${num_cpus} Threads Available for Plotting"
+echo
+echo "Please Edit /usr/local/bin/madmax-plot.sh and Add Your Farmer Key, Your Pool Key, and the Threads to be used by the Plotter"
+echo
+echo "Reboot to start Auto Plotting"
