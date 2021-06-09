de1093b8ba3222021bb676346d92bbb2bd67b408
Rahim Khoja <rahim@khoja.ca>
2021-06-09 14:24:13 -0700

    Update madmax-plot.sh

--- usr/local/bin/madmax-plot.sh
+++ usr/local/bin/madmax-plot.sh
@@ -6,7 +6,7 @@ do
     sleep 10
     
     # Create Plot with 29 Threads (Pool-Key Farmer-Key PlotDir PlotDir Threads[4] Buckets[7])
-    chia_plot a0226aee1c59ba9a70f9c4aab77ea662bce96766b58913dfff2fb48c5766d594d0430b38df7b5f9f1787d39d7ab310f6 9425d29b05b659d05814cd989e0d976c9b755f9730d3e112c8609aa3d5dd250a86ecb8c164a66861857d45e71d6c9b9a /mnt/plot/ /mnt/plot/ 28 7 >> /var/log/chia-plotter/plotter.log
+    chia_plot -p a0226aee1c59ba9a70f9c4aab77ea662bce96766b58913dfff2fb48c5766d594d0430b38df7b5f9f1787d39d7ab310f6 -f 9425d29b05b659d05814cd989e0d976c9b755f9730d3e112c8609aa3d5dd250a86ecb8c164a66861857d45e71d6c9b9a -t /mnt/plot/ -2 /mnt/plot/ -r 28 -u 7 >> /var/log/chia-plotter/plotter.log
 
     # Sleep For 10 Seconds
     sleep 10
