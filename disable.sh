c3ba6a487bef192bd6ae6ddf148a46622fd5a543
Rahim Khoja <rahim@khoja.ca>
2021-06-17 21:25:17 -0700

    Create disable.sh

---
+++ disable.sh
@@ -0,0 +1,6 @@
+sudo systemctl disable swar-watch
+sudo systemctl disable swar-watch.service
+sudo systemctl disable swar-watch.timer
+sudo systemctl disable harvester-watch
+sudo systemctl disable harvester-watch.service
+sudo systemctl disable harvester-watch.timer
